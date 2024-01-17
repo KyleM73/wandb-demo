3f06880f7ade01f97096f555cd64d2f8e359a55d
Kyle Morgenstein <34984693+KyleM73@users.noreply.github.com>
2024-01-17 15:13:34 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -8,25 +8,23 @@ epochs = 5
 
 wandb.init(
     project="demo",
-    
-    # track hyperparameters and run metadata
     config={
     "learning_rate": learning_rate,
-    "architecture": "CNN",
-    "dataset": "CIFAR-10",
+    "architecture": "MLP",
+    "dataset": "MNIST",
     "epochs": epochs,
     "batch_size": batch_size,
     }
 )
 
-training_data = torchvision.datasets.CIFAR10(
+training_data = torchvision.datasets.MNIST(
         root="data",
         train=True,
         download=True,
         transform=torchvision.transforms.ToTensor()
         )
 
-test_data = torchvision.datasets.CIFAR10(
+test_data = torchvision.datasets.MNIST(
         root="data",
         train=False,
         download=True,
@@ -38,19 +36,18 @@ test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
 
 train_len = len(train_dataloader.dataset)
 test_len = len(test_dataloader.dataset)
+
+train_batches = len(train_dataloader)
 test_batches = len(test_dataloader)
 
 class NeuralNetwork(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, inp : int = 784, layers: list = [128,128,128]) -> None:
         super().__init__()
-        self.network = torch.nn.Sequential(
-                torch.nn.Conv2d(3, 6, kernel_size=8, stride=2),
-                torch.nn.ReLU(),
-                torch.nn.Conv2d(6, 12, kernel_size=4, stride=2),
-                torch.nn.ReLU(),
-                torch.nn.Flatten(),
-                torch.nn.Linear(300, 10)
-                )
+        layer_list = [torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(inp, layers[0]), torch.nn.ReLU())]
+        for i in range(len(layers)-1):
+                layer_list.append(torch.nn.Sequential(torch.nn.Linear(layers[i],layers[i+1]), torch.nn.ReLU()))
+        layer_list.append(torch.nn.Sequential(torch.nn.Linear(layers[-1], 10), torch.nn.Softmax(dim=1)))
+        self.network = torch.nn.Sequential(*layer_list)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.network(x)
@@ -64,26 +61,39 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 for e in range(epochs):
         print("Epoch {}\n-------------------------------".format(e+1))
         model.train()
+        train_loss, train_correct = 0, 0
         for batch, (x,y) in enumerate(train_dataloader):
                 pred = model(x)
                 loss = loss_fn(pred, y)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
+                train_loss += loss.item()
+                train_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
                 if batch % 100 == 0:
                         loss, current = loss.item(), batch * len(x)
                         print("Loss: {} [{}%]".format( loss, str(100*current/train_len)[:4]))
+        train_loss /= train_batches
+        train_correct /= train_len
+        print("[Train] Accuracy: {}%, Loss: {} \n".format(str(100*train_correct)[:4], train_loss))
         model.eval()
-        test_loss, correct = 0, 0
+        test_loss, test_correct = 0, 0
         with torch.no_grad():
                 for x,y in test_dataloader:
                         pred = model(x)
                         test_loss += loss_fn(pred, y).item()
-                        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+                        test_correct += (pred.argmax(1) == y).type(torch.float).sum().item()
         test_loss /= test_batches
-        correct /= test_len
-        print("[Test] Accuracy: {}%, Loss: {} \n".format(str(100*correct)[:4], test_loss))
+        test_correct /= test_len
+        print("[Test] Accuracy: {}%, Loss: {} \n".format(str(100*test_correct)[:4], test_loss))
         
-        wandb.log({"acc": 100*correct, "loss": test_loss})
+        wandb.log(
+                {
+                        "train_acc" : train_correct,
+                        "train_loss" : train_loss,
+                        "test_acc": test_correct,
+                        "test_loss": test_loss,
+                        }
+                )
                                 
 wandb.finish()
